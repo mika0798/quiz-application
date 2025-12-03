4de63d230333e57d49ae6f60db88091902d250f5
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-03 13:41:17 +0700

    Complete user service

--- src/main/java/com/project/quiz_application/service/QuizUserDetailsService.java
+++ src/main/java/com/project/quiz_application/service/QuizUserDetailsService.java
@@ -1,4 +1,49 @@
 package com.project.quiz_application.service;
 
-public class QuizUserDetailsService {
+import com.project.quiz_application.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class QuizUserDetailsService implements UserDetailsService {
+    private final Map<String, User> users = new HashMap<>();
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.get(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username " + username + " not found");
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+
+    public void registerUser(User user) throws Exception {
+        if (users.containsKey(user.getUsername())) {
+            throw new Exception("Username " + user.getUsername() + " already exists");
+        } else {
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+            users.put(user.getUsername(), user);
+        }
+
+    }
+
+    public User findByUsername(String username) {
+        return users.get(username);
+    }
+
 }
