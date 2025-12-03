a6fde0eebf3f75325d369522a66d4ed315fd1778
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-03 21:36:40 +0700

    Add optionsAsString field

--- src/main/java/com/project/quiz_application/model/Question.java
+++ src/main/java/com/project/quiz_application/model/Question.java
@@ -1,27 +1,41 @@
 package com.project.quiz_application.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Question {
     private Integer id;
+    @NotBlank(message="Question text cannot be blank")
     private String questionText;
-    private ArrayList<String> options;
+
+    @NotEmpty(message="Options list cannot be blank")
+    private ArrayList<String> options = new ArrayList<>();
+
+    @NotBlank(message="Answer cannot be blank")
     private String answer;
 
     public String getOptionsAsString() {
-        return String.join(",", options);
+        return options == null ? "" : String.join(",", options);
     }
 
-    public void setOptionsFromString(String options) {
-        this.options = new ArrayList<>(Arrays.asList(options.split(",")));
+    public void setOptionsAsString(String optionStr) {
+        if (optionStr == null || optionStr.equals("")) {
+            this.options = new ArrayList<>();
+        } else {
+            this.options = Arrays.stream(optionStr.split("\\s*,\\s*"))
+                    .filter(s -> !s.isEmpty())
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
     }
 
     @Override
