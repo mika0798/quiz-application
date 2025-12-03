7431ca65914f2ec99a97c4fa320cc2472106bbbd
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-03 21:36:48 +0700

    fix redirect

--- src/main/java/com/project/quiz_application/controller/QuizAppController.java
+++ src/main/java/com/project/quiz_application/controller/QuizAppController.java
@@ -4,6 +4,7 @@ import com.project.quiz_application.model.Question;
 import com.project.quiz_application.model.User;
 import com.project.quiz_application.service.QuestionService;
 import com.project.quiz_application.service.QuizUserDetailsService;
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -16,24 +17,49 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class QuizAppController {
     private final AuthenticationManager authenticationManager;
     private final QuizUserDetailsService userDetailsService;
     private final QuestionService questionService;
+
+    @PostConstruct
+    public void init() throws Exception {
+        Question question1 = new Question(
+                questionService.getNextId(),
+                "What is the first letter in the alphabet?",
+                new ArrayList<>(Arrays.asList("D","A","Y","W")),
+                "A"
+                );
+
+        Question question2 = new Question(
+                questionService.getNextId(),
+                "What year was Hitler born in?",
+                new ArrayList<>(Arrays.asList("1898","1881","1889","1890")),
+                "1889"
+        );
+        questionService.addQuiz(question1);
+        questionService.addQuiz(question2);
+        userDetailsService.registerUser(new User("admin","123","admin@email.com","ADMIN"));
+        userDetailsService.registerUser(new User("user","123","user@gmail.com","USER"));
+    }
 
     @Autowired
     public QuizAppController(AuthenticationManager authenticationManager, QuizUserDetailsService userDetailsService) {
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
         this.questionService = new QuestionService();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
     }
 
     /*---- Login and Register API ----*/
@@ -51,7 +77,7 @@ public class QuizAppController {
     public String home(Model model) {
         Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
         if (auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
-            return "redirect:login";
+            return "redirect:/login";
         }
         String username = auth.getName();
         model.addAttribute("username", username);
@@ -63,41 +89,119 @@ public class QuizAppController {
 
         if (role.equals("ROLE_ADMIN")) {
             List<Question> questions = questionService.getQuizzesList();
-            model.addAttribute("quizzes", questions);
+            model.addAttribute("questions", questions);
             return "quizlist";
         } else {
             List<Question> questions = questionService.getQuizzesList();
-            model.addAttribute("quizzes", questions);
+            model.addAttribute("questions", questions);
             return "quiz";
         }
     }
 
     @PostMapping("/register")
     public String registerUser(
-            @Valid User registerUser,
+            @Valid @ModelAttribute("newUser") User registerUser,
             BindingResult bindingResult
     ) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        String rawPassword = registerUser.getPassword();
         try {
-            if (bindingResult.hasErrors()) {
-                return "register";
-            }
-
             userDetailsService.registerUser(registerUser);
         } catch (Exception userAlreadyExists) {
             return "redirect:/register?error";
         }
 
         Authentication auth = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(registerUser.getUsername(), registerUser.getPassword())
-        );
+            new UsernamePasswordAuthenticationToken(registerUser.getUsername(), rawPassword));
 
         SecurityContextHolder.getContext().setAuthentication(auth);
-
         return  "redirect:/login?success";
-
     }
 
     /* There's no need to write POST API for login service,
     security filter chain already takes care of that :))
      */
+    /*---- Adding Quiz GET, POST APIs ----*/
+
+    /*---- Adding Quiz GET, POST APIs ----*/
+    @GetMapping("/addquiz")
+    public String showAddQuiz(Model model) {
+        model.addAttribute("question",new Question());
+        return "addquiz";
+    }
+
+    @PostMapping("/addquiz")
+    public String addQuiz(
+            @ModelAttribute @Valid Question question,
+            BindingResult bindingResult,
+            Model model,
+            Authentication auth) {
+        if (bindingResult.hasErrors()) {
+            return "addquiz";
+        }
+
+        String role = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse("ROLE_USER");
+        if (role.equals("ROLE_ADMIN")) {
+            question.setId(questionService.getNextId());
+            if (questionService.addQuiz(question)) {
+                model.addAttribute("success","Added question successfully!");
+                return "redirect:/addquiz?success";
+            } else {
+                model.addAttribute("error","Failed to add question!");
+                return "redirect:/addquiz?error";
+            }
+
+        } else {
+            model.addAttribute("error","You do not have permission to do this!");
+            return "redirect:/home";
+        }
+    }
+
+    /*---- Editing Quiz GET, POST APIs ----*/
+    @GetMapping("/editquiz/{id}")
+    public String editQuiz(@PathVariable("id") int id, Model model) {
+        model.addAttribute("question",questionService.getQuizById(id));
+        return "editquiz";
+    }
+
+    @PostMapping("/editquiz")
+    public String editQuiz(
+            @ModelAttribute("question") @Valid Question question,
+            BindingResult bindingResult,
+            Model model,
+            Authentication auth
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "editquiz";
+        }
+
+        String role = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse("ROLE_USER");
+        if (role.equals("ROLE_ADMIN")) {
+            if (questionService.editQuiz(question)) {
+                model.addAttribute("success","Edited question successfully!");
+                return "redirect:/home?success";
+            } else {
+                model.addAttribute("error","Cannot edit question!");
+                return "redirect:/edit/{id}?error";
+            }
+
+        } else  {
+            model.addAttribute("error","You do not have permission to do this!");
+            return "redirect:/home";
+        }
+    }
+
+
+    /*---- Deleting Quiz API ----*/
+
+    /*---- Submitting API ----*/
+
 }
