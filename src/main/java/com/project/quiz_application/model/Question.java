c61a86cfe9d63a3692eb63e5d5c610ea694ed248
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-03 13:41:07 +0700

    Complete all models

--- src/main/java/com/project/quiz_application/model/Question.java
+++ src/main/java/com/project/quiz_application/model/Question.java
@@ -1,4 +1,34 @@
 package com.project.quiz_application.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Question {
+    private Integer id;
+    private String questionText;
+    private ArrayList<String> options;
+    private String answer;
+
+    public String getOptionsAsString() {
+        return String.join(",", options);
+    }
+
+    public void setOptionsFromString(String options) {
+        this.options = new ArrayList<>(Arrays.asList(options.split(",")));
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id
+                + "\nQuestion: " +  questionText
+                +"\nOptions: " + getOptionsAsString()
+                +"\nCorrect answer: " + answer;
+    }
 }
--- src/main/java/com/project/quiz_application/model/User.java
+++ src/main/java/com/project/quiz_application/model/User.java
@@ -1,4 +1,32 @@
 package com.project.quiz_application.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @NotBlank(message="Username is required")
+    private String username;
+
+    @NotBlank(message="Password cannot be empty")
+    private String password;
+
+    @NotBlank(message="Email is required")
+    @Email(message="Invalid email format")
+    private String email;
+
+    private String role;
+
+    @Override
+    public String toString() {
+        return "Username: " + username
+                + ", Email: " + email
+                + ", Role: " + role;
+    }
 }
