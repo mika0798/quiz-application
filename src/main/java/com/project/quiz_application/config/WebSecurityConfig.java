d600d0b17c47c8ed108ef079cdaa1af757840447
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-03 14:38:25 +0700

    -Create websec config -Add password encoder -Add authentication manager

--- src/main/java/com/project/quiz_application/config/WebSecurityConfig.java
+++ src/main/java/com/project/quiz_application/config/WebSecurityConfig.java
@@ -1,4 +1,41 @@
 package com.project.quiz_application.config;
 
+import com.project.quiz_application.service.QuizUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig {
+    private final QuizUserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(QuizUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+            http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+        return authenticationManagerBuilder.build();
+    }
+
+
+
 }
